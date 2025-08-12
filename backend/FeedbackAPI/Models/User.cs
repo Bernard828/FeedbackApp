7a9a35e5a6f74d82cd8afffc7df945bb45ad40bf
Bernard828 <bernard.lackey1@gmail.com>
2025-08-12 11:07:36 -0400

    Roles added to User Model for association/relation

--- backend/FeedbackAPI/Models/User.cs
+++ backend/FeedbackAPI/Models/User.cs
@@ -7,5 +7,7 @@
         public string Email { get; set; } = string.Empty;
         public string PasswordHash { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public ICollection<Role> Roles { get; set; } = new List<Role>();
     }
 }
